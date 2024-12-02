c72458c515513a27855c561daabf18e87df9dd18
perdreauarthur <arthur.perdreau@outlook.fr>
2024-12-02 21:13:11 +0100

    30 min of the video. New notion for me : fill, many parameter in querySelectorAll, .target, .parentNode

--- js/main.js
+++ js/main.js
@@ -19,6 +19,9 @@ function createBoard(){
         const square=document.createElement("div");
         square.classList.add("square");
         square.innerHTML=startPiece;
+        if(square.firstChild){
+            square.firstChild.setAttribute("draggable",true);
+        }
         square.setAttribute("squareId",i);
         const row=Math.floor((63-i) / 8)+ 1;
         if (row%2===0){
@@ -26,8 +29,25 @@ function createBoard(){
         }else{
             square.classList.add(i%2===0 ?"brown":"beige");
         }
+        if(i<=15){
+            square.firstChild.firstChild.classList.add("black");
+        }
+        if(i>=48){
+            square.firstChild.firstChild.classList.add("white");
+        }
         gameBoard.append(square);
     })
 }
 createBoard();
-//21 min
+
+const allSquares= document.querySelectorAll("#gameBoard .square")//--> on peut mettre plusieurs class ou id
+allSquares.forEach((square)=>{
+    square.addEventListener("dragstart",dragStart)
+})
+let startPositionId
+let draggedElement
+function dragStart(e){//--> le e correspond à l'élément qui appelle la fonction
+    startPositionId=e.target.parentNode.getAttribute("squareId")
+    draggedElement=e.target;
+}
+//30min

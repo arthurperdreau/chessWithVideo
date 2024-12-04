94c96e621d6679aa8c4a7351ec95ef4c89a44adc
perdreauarthur <arthur.perdreau@outlook.fr>
2024-12-04 12:04:04 +0100

    46 min of the video. New notion for me : .stopPropagation, .preventDefault

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
 const gameBoard = document.querySelector("#gameBoard");
-const playerDisplay = document.querySelector("#playerDisplay");
+const playerDisplay = document.querySelector("#player");
 const infoDisplay = document.querySelector("#infoDisplay");
 const width=8
+let playerGo="black"
+playerDisplay.textContent="black"
 
 
 const startPieces=[
-    rook,knight,bishop,queen,king,bishop,king,rook,
+    rook,knight,bishop,queen,king,bishop,knight,rook,
     pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
     "","","","","","","","",
     "","","","","","","","",
     "","","","","","","","",
     "","","","","","","","",
     pawn,pawn,pawn,pawn,pawn,pawn,pawn,pawn,
-    rook,knight,bishop,queen,king,bishop,king,rook,
+    rook,knight,bishop,queen,king,bishop,knight,rook,
 ]
 function createBoard(){
     startPieces.forEach((startPiece, i)=>{ //--> la boucle forEach permet aussi d'obtenir les indices
@@ -40,14 +42,52 @@ function createBoard(){
 }
 createBoard();
 
-const allSquares= document.querySelectorAll("#gameBoard .square")//--> on peut mettre plusieurs class ou id
+const allSquares= document.querySelectorAll(".square")//--> on peut mettre plusieurs class ou id
 allSquares.forEach((square)=>{
     square.addEventListener("dragstart",dragStart)
+    square.addEventListener("dragover",dragOver)
+    square.addEventListener("drop",dragDrop)
+
 })
 let startPositionId
 let draggedElement
 function dragStart(e){//--> le e correspond à l'élément qui appelle la fonction
     startPositionId=e.target.parentNode.getAttribute("squareId")
     draggedElement=e.target;
+}
+function dragOver(e){
+    e.preventDefault()//--> permet d'enpecher l'action. Par exemple link.preventDefault() ne va pas ouvrir le lien
+}
+function dragDrop(e){
+    e.stopPropagation()//--> stop les événements ou actions suivants associés à e
+    const correctGo=draggedElement.firstChild.classList.contains(playerGo);
+    const taken=e.target.classList.contains("piece")
+    const opponentGo=playerGo==="white"?"black":"white"
+    const takenByOpponent=e.target.firstChild?.classList.contains(opponentGo);
+    //e.target.parentNode.append(draggedElement)
+    //e.target.remove()
+    changePlayer()
+}
+function changePlayer(){
+    if(playerGo==="black"){
+        reverseIds()
+        playerGo="white"
+        playerDisplay.textContent="white"
+    }else{
+        revertIds()
+        playerGo="black"
+        playerDisplay.textContent="black"
+    }
+
+}
+function reverseIds(){
+    const allSquares=document.querySelectorAll(".square")
+    allSquares.forEach((square,i)=>
+        square.setAttribute("squareId",(width*width-1)-i))
+}
+function revertIds(){
+    const allSquares=document.querySelectorAll(".square")
+    allSquares.forEach((square,i)=>
+        square.setAttribute("squareId",i))
 }
 //30min
